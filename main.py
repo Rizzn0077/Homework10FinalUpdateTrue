f24b3f8801703206b2097104ff6bf4c696a40189
Rizzn0077 <111635546+Rizzn0077@users.noreply.github.com>
2022-09-01 14:16:29 +0300

    Homework 10

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 from x_and_y import *
 
-plt.plot(x1, y1, label="Лінія 1")
-plt.plot(x2, y2, label="Лінія 2")
-plt.plot(x3, y3, label="Лінія 3")
+plt.plot(x_var(), y_var(), label="Лінія 1")
+plt.plot(x_var2(), y_var2(), label="Лінія 2")
+plt.plot(x_var3(), y_var3(), label="Лінія 3")
+plt.plot(x_var4(), y_var4(), label="Лінія 4")
 plt.xlabel('Сторона X')
 plt.ylabel('Сторона X')
 plt.title('Домашня Робота')
--- x_and_y.py
+++ x_and_y.py
@@ -1,6 +1,89 @@
-x1 = [1, 2, 3, 4, 5, 6]
-y1 = [2, 4, 1, 5, 2, 6]
-x2 = [1.2, 2.2, 3.2, 4.2, 5.2, 6.2]
-y2 = [2.2, 4.2, 1.2, 5.2, 2.2, 6.2]
-x3 = [1.4, 2.4, 3.4, 4.4, 5.4, 6.4]
-y3 = [2.4, 4.4, 1.4, 5.4, 2.4, 6.4]
+import random
+
+
+def x_var():
+    x1 = random.randint(1, 7)
+    x2 = random.randint(1, 7)
+    x3 = random.randint(1, 7)
+    x4 = random.randint(1, 7)
+    x5 = random.randint(1, 7)
+    x6 = random.randint(1, 7)
+    x7 = random.randint(1, 7)
+    return x1, x2, x3, x4, x5, x6, x7
+
+
+def y_var():
+    y1 = random.randint(1, 7)
+    y2 = random.randint(1, 7)
+    y3 = random.randint(1, 7)
+    y4 = random.randint(1, 7)
+    y5 = random.randint(1, 7)
+    y6 = random.randint(1, 7)
+    y7 = random.randint(1, 7)
+    return y1, y2, y3, y4, y5, y6, y7
+
+
+def x_var2():
+    x1 = random.randint(1, 7)
+    x2 = random.randint(1, 7)
+    x3 = random.randint(1, 7)
+    x4 = random.randint(1, 7)
+    x5 = random.randint(1, 7)
+    x6 = random.randint(1, 7)
+    x7 = random.randint(1, 7)
+    return x1, x2, x3, x4, x5, x6, x7
+
+
+def y_var2():
+    y1 = random.randint(1, 7)
+    y2 = random.randint(1, 7)
+    y3 = random.randint(1, 7)
+    y4 = random.randint(1, 7)
+    y5 = random.randint(1, 7)
+    y6 = random.randint(1, 7)
+    y7 = random.randint(1, 7)
+    return y1, y2, y3, y4, y5, y6, y7
+
+
+def x_var3():
+    x1 = random.randint(1, 7)
+    x2 = random.randint(1, 7)
+    x3 = random.randint(1, 7)
+    x4 = random.randint(1, 7)
+    x5 = random.randint(1, 7)
+    x6 = random.randint(1, 7)
+    x7 = random.randint(1, 7)
+    return x1, x2, x3, x4, x5, x6, x7
+
+
+def y_var3():
+    y1 = random.randint(1, 7)
+    y2 = random.randint(1, 7)
+    y3 = random.randint(1, 7)
+    y4 = random.randint(1, 7)
+    y5 = random.randint(1, 7)
+    y6 = random.randint(1, 7)
+    y7 = random.randint(1, 7)
+    return y1, y2, y3, y4, y5, y6, y7
+
+
+def x_var4():
+    x1 = random.randint(1, 7)
+    x2 = random.randint(1, 7)
+    x3 = random.randint(1, 7)
+    x4 = random.randint(1, 7)
+    x5 = random.randint(1, 7)
+    x6 = random.randint(1, 7)
+    x7 = random.randint(1, 7)
+    return x1, x2, x3, x4, x5, x6, x7
+
+
+def y_var4():
+    y1 = random.randint(1, 7)
+    y2 = random.randint(1, 7)
+    y3 = random.randint(1, 7)
+    y4 = random.randint(1, 7)
+    y5 = random.randint(1, 7)
+    y6 = random.randint(1, 7)
+    y7 = random.randint(1, 7)
+    return y1, y2, y3, y4, y5, y6, y7
