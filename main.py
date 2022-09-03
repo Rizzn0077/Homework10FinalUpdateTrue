603d282750b4b605fb005c0324e888a5aac08ca1
Rizzn0077 <111635546+Rizzn0077@users.noreply.github.com>
2022-09-03 11:00:34 +0300

    Homework 10

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
 import matplotlib.pyplot as plt
 from x_and_y import *
+x = int(input('Input X: '))
+y = int(input('Input Y: '))
+color_input = input('Input Color HEX Code / Color Name: ')
 
-for _ in range(4):
-    which_plot = input("""Which plot would you like to see?
-    Plot numbers: [1, 2, 3]
-    Input plot number: """)
-    print('=' * 50)
-    if which_plot == '1':
-        plt.plot(generate_x_and_y(0, 0), label='Line - 1', color='red')
-        plt.plot(generate_x_and_y(0, 0), label='Line - 2', color='blue')
-        plt.plot(generate_x_and_y(0, 0), label='Line - 3', color='yellow')
-        plt.plot(generate_x_and_y(0, 0), label='Line - 4', color='black')
-        plt.xlabel('Label - X')
-        plt.ylabel('Label - Y')
-        plt.title("Homework - Plot 1")
-        plt.legend()
-        plt.show()
-    elif which_plot == '2':
-        plt.plot(generate_x_and_y2(0, 0), label='Line - 1', color='red')
-        plt.plot(generate_x_and_y2(0, 0), label='Line - 2', color='blue')
-        plt.xlabel('Label - X')
-        plt.ylabel('Label - Y')
-        plt.title("Homework - Plot 2")
-        plt.legend()
-        plt.show()
-    elif which_plot == '3':
-        plt.plot(generate_x_and_y3(0, 0), label='Line - 1', color='black')
-        plt.xlabel('Label - X')
-        plt.ylabel('Label - Y')
-        plt.title("Homework - Plot 3")
-        plt.legend()
-        plt.show()
-    else:
-        print("Plot doesn't exist.")
-        print('=' * 50)
+plt.plot(generate_x_and_y(x, y), label='Line - 1', color=color_input)
+plt.xlabel('Label - X')
+plt.ylabel('Label - Y')
+plt.title("Homework")
+plt.legend()
+plt.show()
