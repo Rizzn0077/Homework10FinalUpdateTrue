c683cd679ed07765995918d61142e7771815c338
Rizzn0077 <111635546+Rizzn0077@users.noreply.github.com>
2022-09-03 10:31:42 +0300

    Homework 10

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from x_and_y import *
 
-x = np.linspace(0, generate_x_and_y(x=1, y=3))
-y = np.linspace(0, generate_x_and_y(x=5, y=8))
-
-plt.figure(figsize=(5, 2.7), layout='constrained')
-plt.plot(x, y, label='line')
-plt.xlabel('x label')
-plt.ylabel('y label')
-plt.title("Homework")
-plt.legend()
-plt.show()
+for _ in range(4):
+    which_plot = input("""Which plot would you like to see?
+    Plot numbers: [1, 2, 3]
+    Input plot number: """)
+    print('=' * 50)
+    if which_plot == '1':
+        plt.plot(generate_x_and_y(0, 0), label='Line - 1', color='red')
+        plt.plot(generate_x_and_y(0, 0), label='Line - 2', color='blue')
+        plt.plot(generate_x_and_y(0, 0), label='Line - 3', color='yellow')
+        plt.plot(generate_x_and_y(0, 0), label='Line - 4', color='black')
+        plt.xlabel('Label - X')
+        plt.ylabel('Label - Y')
+        plt.title("Homework - Plot 1")
+        plt.legend()
+        plt.show()
+    elif which_plot == '2':
+        plt.plot(generate_x_and_y2(0, 0), label='Line - 1', color='red')
+        plt.plot(generate_x_and_y2(0, 0), label='Line - 2', color='blue')
+        plt.xlabel('Label - X')
+        plt.ylabel('Label - Y')
+        plt.title("Homework - Plot 2")
+        plt.legend()
+        plt.show()
+    elif which_plot == '3':
+        plt.plot(generate_x_and_y3(0, 0), label='Line - 1', color='black')
+        plt.xlabel('Label - X')
+        plt.ylabel('Label - Y')
+        plt.title("Homework - Plot 3")
+        plt.legend()
+        plt.show()
+    else:
+        print("Plot doesn't exist.")
+        print('=' * 50)
--- x_and_y.py
+++ x_and_y.py
@@ -4,7 +4,28 @@ import random
 def generate_x_and_y(x, y):
     if x < y:
         x, y = x, y
-    x_y_list = [x, y]
+    x_y_list = []
+    for _ in range(8):
+        num = random.randint(1, 150)
+        x_y_list.append(num)
+    return x_y_list[0], x_y_list[1], x_y_list[2], x_y_list[3], x_y_list[4], x_y_list[5], x_y_list[6], x_y_list[7]
+
+
+def generate_x_and_y2(x, y):
+    if x < y:
+        x, y = x, y
+    x_y_list = []
     for _ in range(2):
-        random.randint(1, 20)
+        num = random.randint(1, 150)
+        x_y_list.append(num)
     return x_y_list[0], x_y_list[1]
+
+
+def generate_x_and_y3(x, y):
+    if x < y:
+        x, y = x, y
+    x_y_list = []
+    for _ in range(3):
+        num = random.randint(1, 150)
+        x_y_list.append(num)
+    return x_y_list[0], x_y_list[1], x_y_list[2]
